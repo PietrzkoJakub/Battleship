493b6071d570a55197e37afe46f5421565751791
Jakub Pietrzko <pietrzko,jakub@gmail.com>
2021-05-09 17:20:12 +0200

    skeleton

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import sys
-import pygame
-from pygame.locals import *
-from game import*
+from tkinter import *
 
 """
-This is the main file, where with game loop
+This is the main file, with game loop
 """
 
+def mainWindowInit():
+    display = Tk()
+    display.resizable(width = True, height = True)
 
-game = Game()
+    welcome = Label(display,text = "Welcome")
+    welcome.grid(row = 0, column= 0) #text location on the window(table)
+
+    #Frame
+    frame = Frame(display,borderwidth = 4)
+    frame.grid(row=0, column=1)
+    frame.config(background='black')
+    lab1 = Label(frame, text='Ramka')
+    lab1.grid(sticky=E, row=0, column=0, padx=5, pady=5)
+    display.mainloop()
+
+
+
+gameOver = False
 
 if __name__ == "__main__":
-    while(not game.gameOver):
-        game.checkEvents()
+    while(not gameOver):
+        #mainWindowInit()
+        input("Press return to exit")
+        gameOver = True
 
 
 

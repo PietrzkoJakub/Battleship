a1ff3f1d827c7322311d7b0ac611925c74ca8d19
Jakub Pietrzko <pietrzko,jakub@gmail.com>
2021-05-13 17:23:30 +0200

    Code refactor and button checker

---
+++ buttonsChecker.py
@@ -0,0 +1,15 @@
+import sys
+from tkinter import *
+
+
+class Checkers():
+    def __init__(self,root):
+        self.buttonsDict = {}
+        self.root = root
+    def create_button(self, setBg, x, y):
+        button = Button(self.root, bg=setBg)
+        button.place(x=x, y=y, height=50, width=50)
+        self.buttonsDict[(x, y)] = button
+
+    def update_button(self, x, y, text):
+        self.buttonsDict[(x, y)].configure(text=text)
--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 import sys
 from tkinter import *
+from buttonsChecker import *
 
 """
 This is the main file, with game loop
 """
 
-display = Tk()
+M = 20
+N = 10
+
 gameOver = False
-heightRes = 1080
-widthRes = 1920
+heightRes = 800
+widthRes = 1200
 sizeConst = 0.035
 alphabet = [chr(i) for i in range(ord('A'),ord('J')+1)]
+buttonVerticalSize = 50
+buttonHorizontalSize = 50
+
+
+
+
+def rootInit():
+    root = Tk()
+    root.geometry(str(widthRes) + "x" + str(heightRes))
+    root.resizable(width=True, height=True)
+    root.title("Battleship")
+    return root
 
 def exit():
     sys.exit(1)
@@ -22,12 +37,11 @@ def resetGame():
     pass
 
 def mainWindowInit():
-    display.geometry(str(widthRes) + "x" + str(heightRes))
-    display.resizable(width = True, height = True)
+    root = rootInit()
     mainButtonsInit()
-    mapButtonsInit()
-    fieldMarks(alphabet)
-    display.mainloop()
+    fieldMarks(root,alphabet)
+    mapButtonsInit(root)
+    root.mainloop()
 
 def mainButtonsInit():
     button1 = Button(text="New Game", command=newGame)
@@ -36,52 +50,49 @@ def mainButtonsInit():
     button2 = Button(text="Reset Game", command=resetGame())
     button2.place(x= 0, y=25, height = 25, width =80)
 
-
     button3 = Button(text="Exit", command=exit)
     button3.place(x= 0, y=50, height = 25, width =80)
 
     testButton = Button(text = "test")
     testButton.place(x=500, y=500, height = 50, width =50)
 
+def mapButtonsInit(root):
+    checker = Checkers(root)
+    buttonCreate(checker,"yellow",100,600)
+    buttonCreate(checker,"red",650,1150)
 
-
-buttonVerticalSize = 50
-buttonHorizontalSize = 50
-
-def mapButtonsInit():
-    for i in range(100,600,50):
-        for j in range(100,600,50):
-            button = Button(bg = "yellow")
-            button.place(x=i, y=j, height=50, width=50)
-
-    for i in range(650, 1150, 50):
+def buttonCreate(checker,color,start,end):
+    for i in range(start, end, 50):
         for j in range(100, 600, 50):
-            button = Button(bg="red")
-            button.place(x=i, y=j, height=50, width=50)
+            checker.create_button(color, i, j)
 
-def fieldMarks(letters):
+
+
+def fieldFillWithLetters(root, letters, startLocation, endLocation):
     j = 0
-    for i in range(int(100+buttonVerticalSize/2),int(600+buttonVerticalSize/2),50):
-        label = Label(display,text = letters[j]).place(x=i,y=75)
+    for i in range(int(startLocation + buttonVerticalSize / 2), int(endLocation + buttonVerticalSize / 2), 50):
+        label = Label(root, text=letters[j]).place(x=i, y=75)
         j += 1
-    j = 0
-    for i in range(int(650 + buttonVerticalSize / 2), int(1150 + buttonVerticalSize / 2), 50):
-        label = Label(display, text=letters[j]).place(x=i, y=75)
-        j += 1
+
+def fieldFillWithNumbers(root,xCordinate):
     j = 1
     for i in range(int(100 + buttonVerticalSize / 2), int(600 + buttonVerticalSize / 2), 50):
-        label = Label(display, text=str(j)).place(x=75, y=i-5)
-        j += 1
-    j = 1
-    for i in range(int(100 + buttonVerticalSize / 2), int(600 + buttonVerticalSize / 2), 50):
-        label = Label(display, text=str(j)).place(x=625, y=i-5)
+        label = Label(root, text=str(j)).place(x=xCordinate, y=i - 5)
         j += 1
 
 
+def fieldMarks(root, letters):
+    fieldFillWithLetters(root, letters, 100, 600)
+    fieldFillWithLetters(root, letters, 650, 1150)
+    fieldFillWithNumbers(root, 75)
+    fieldFillWithNumbers(root, 625)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     mainWindowInit()
+
+
+
 
 
 

8e272176109f1835aa07797e12b9543eaa854ddd
Jakub Pietrzko <pietrzko,jakub@gmail.com>
2021-05-14 13:50:11 +0200

    adding button functions, currently buttonsChecker is inactive

--- buttonsChecker.py
+++ buttonsChecker.py
@@ -2,7 +2,7 @@ import sys
 from tkinter import *
 
 
-class Checkers():
+class Checkers:
     def __init__(self,root):
         self.buttonsDict = {}
         self.root = root
@@ -11,5 +11,8 @@ class Checkers():
         button.place(x=x, y=y, height=50, width=50)
         self.buttonsDict[(x, y)] = button
 
-    def update_button(self, x, y, text):
-        self.buttonsDict[(x, y)].configure(text=text)
+    def update_button(self, x, y,command):
+        self.buttonsDict[(x, y)]["command"] = command
+
+    def change_color(self,x,y,color):
+        self.buttonsDict[(x,y)].configure(bg = color)
--- main.py
+++ main.py
@@ -5,10 +5,7 @@ from buttonsChecker import *
 """
 This is the main file, with game loop
 """
-
-M = 20
-N = 10
-
+"""
 gameOver = False
 heightRes = 800
 widthRes = 1200
@@ -17,15 +14,24 @@ alphabet = [chr(i) for i in range(ord('A'),ord('J')+1)]
 buttonVerticalSize = 50
 buttonHorizontalSize = 50
 
-
-
-
+"""
+"""
 def rootInit():
     root = Tk()
     root.geometry(str(widthRes) + "x" + str(heightRes))
     root.resizable(width=True, height=True)
     root.title("Battleship")
     return root
+
+"""
+
+"""
+root = Tk()
+root.geometry(str(widthRes) + "x" + str(heightRes))
+root.resizable(width=True, height=True)
+root.title("Battleship")
+checker = Checkers(root)
+
 
 def exit():
     sys.exit(1)
@@ -37,27 +43,32 @@ def resetGame():
     pass
 
 def mainWindowInit():
-    root = rootInit()
-    mainButtonsInit()
+    #root = rootInit()
+    #checker = Checkers(root)
+    enemyTable = gameTable()
+    playerTable = gameTable()
     fieldMarks(root,alphabet)
-    mapButtonsInit(root)
+    mapButtonsInit(root, checker)
+    mainButtonsInit(checker)
     root.mainloop()
 
-def mainButtonsInit():
+
+def mainButtonsInit(checker:Checkers):
     button1 = Button(text="New Game", command=newGame)
     button1.place(x=0, y=0, height=25, width=80)
 
-    button2 = Button(text="Reset Game", command=resetGame())
+    button2 = Button(text="Reset Game", command =resetGame)
     button2.place(x= 0, y=25, height = 25, width =80)
 
     button3 = Button(text="Exit", command=exit)
     button3.place(x= 0, y=50, height = 25, width =80)
 
-    testButton = Button(text = "test")
-    testButton.place(x=500, y=500, height = 50, width =50)
 
-def mapButtonsInit(root):
-    checker = Checkers(root)
+def something():
+    checker.change_color(200,200,"blue")
+
+
+def mapButtonsInit(root, checker):
     buttonCreate(checker,"yellow",100,600)
     buttonCreate(checker,"red",650,1150)
 
@@ -65,8 +76,6 @@ def buttonCreate(checker,color,start,end):
     for i in range(start, end, 50):
         for j in range(100, 600, 50):
             checker.create_button(color, i, j)
-
-
 
 def fieldFillWithLetters(root, letters, startLocation, endLocation):
     j = 0
@@ -87,6 +96,95 @@ def fieldMarks(root, letters):
     fieldFillWithNumbers(root, 75)
     fieldFillWithNumbers(root, 625)
 
+def gameTable():
+    gameTable = [[0 for j in range(10)] for i in range(10)]
+    return gameTable
+
+if __name__ == '__main__':
+    mainWindowInit()
+
+
+"""
+def buttonCreate(root,color,start,end):
+    buttons = {}
+    for i in range(start, end, 50):
+        for j in range(100, 600, 50):
+            button = Button(root, bg=color)
+            button.bind('<Button-1>',lambda event,b = button:changeColor(b))
+            button.place(x=i, y=j, height=50, width=50)
+            buttons[(i, j)] = button
+    return buttons
+
+gameOver = False
+heightRes = 800
+widthRes = 1200
+alphabet = [chr(i) for i in range(ord('A'),ord('J')+1)]
+
+def rootInit():
+    root = Tk()
+    root.geometry(str(widthRes) + "x" + str(heightRes))
+    root.resizable(width=True, height=True)
+    root.title("Battleship")
+    return root
+
+
+def exit():
+    sys.exit(1)
+
+def newGame():
+    pass
+
+def resetGame():
+    pass
+
+def mainWindowInit():
+    root = rootInit()
+    fieldMarks(root,alphabet)
+    playerButtons = buttonCreate(root, "yellow", 100, 600)
+    enemyButtons = buttonCreate(root, "red", 650, 1150)
+    mainButtonsInit()
+    root.mainloop()
+
+
+def mainButtonsInit():
+    button1 = Button(text="New Game")
+    button1.place(x=0, y=0, height=25, width=80)
+
+    button2 = Button(text="Reset Game", command =resetGame)
+    button2.place(x= 0, y=25, height = 25, width =80)
+
+    button3 = Button(text="Exit", command=exit)
+    button3.place(x= 0, y=50, height = 25, width =80)
+
+def leftClick():
+    print("Test")
+
+
+def fieldFillWithLetters(root, letters, startLocation, endLocation):
+    j = 0
+    for i in range(int(startLocation + 25), int(endLocation + 25), 50):
+        label = Label(root, text=letters[j]).place(x=i, y=75)
+        j += 1
+
+def changeColor(button:Button):
+    button.configure(bg="blue")
+
+def fieldFillWithNumbers(root,xCordinate):
+    j = 1
+    for i in range(int(100 + 25), int(600 + 25), 50):
+        label = Label(root, text=str(j)).place(x=xCordinate, y=i - 5)
+        j += 1
+
+
+def fieldMarks(root, letters):
+    fieldFillWithLetters(root, letters, 100, 600)
+    fieldFillWithLetters(root, letters, 650, 1150)
+    fieldFillWithNumbers(root, 75)
+    fieldFillWithNumbers(root, 625)
+
+def gameTable():
+    gameTable = [[0 for j in range(10)] for i in range(10)]
+    return gameTable
 
 if __name__ == '__main__':
     mainWindowInit()
